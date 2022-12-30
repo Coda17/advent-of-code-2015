c7c96c72ed69345680b4ffab565f24f70e4ccf15
Steve Hickey <hickeyst@gmail.com>
2022-12-30 00:57:33 -0500

    feat: day 1

---
+++ src/AoC2015/Solvers/Day01.cs
@@ -0,0 +1,28 @@
+﻿namespace AoC2015.Solvers;
+
+public sealed class Day01 : BaseProblem
+{
+    private readonly string _input;
+
+    public Day01()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    protected override string Solve1() => _input.Aggregate(0, (i, c) => i + (c == '(' ? 1 : -1)).ToString();
+
+    protected override string Solve2()
+    {
+        var floor = 0;
+        for (var i = 0; i < _input.Length; ++i)
+        {
+            floor += _input[i] == '(' ? 1 : -1;
+            if (floor == -1)
+            {
+                return $"{i + 1}";
+            }
+        }
+
+        throw new SolveFailedException();
+    }
+}
