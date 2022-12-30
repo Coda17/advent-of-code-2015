bd0d7ba08cf81edb70a7e17456df4963357d5658
Steve Hickey <hickeyst@gmail.com>
2022-12-30 01:35:29 -0500

    feat: day 2

--- src/AoC2015/BaseProblem.cs
+++ src/AoC2015/BaseProblem.cs
@@ -1,13 +1,20 @@
 ﻿namespace AoC2015;
 
-public abstract class BaseProblem : AoCHelper.BaseProblem
+public abstract class BaseProblem<TU> : BaseProblem<TU, TU>
+    where TU : notnull
+{
+}
+
+public abstract class BaseProblem<TU, TV> : AoCHelper.BaseProblem
+    where TU : notnull
+    where TV : notnull
 {
     protected override string ClassPrefix => "Day";
     protected override string InputFileExtension => ".in";
 
-    protected abstract string Solve1();
-    protected abstract string Solve2();
+    protected abstract TU Solve1();
+    protected abstract TV Solve2();
 
-    public override ValueTask<string> Solve_1() => new(Solve1());
-    public override ValueTask<string> Solve_2() => new(Solve2());
+    public override ValueTask<string> Solve_1() => new(Solve1().ToString()!);
+    public override ValueTask<string> Solve_2() => new(Solve2().ToString()!);
 }
---
+++ src/AoC2015/Solvers/Day02.cs
@@ -0,0 +1,22 @@
+﻿namespace AoC2015.Solvers;
+
+public sealed class Day02 : BaseProblem<int>
+{
+    private readonly string[] _input;
+
+    public Day02()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+
+    protected override int Solve1() => _input
+        .Select(l => l.Split('x').Select(int.Parse).ToArray())
+        .Select(d => new[] { 2 * d[0] * d[1], 2 * d[1] * d[2], 2 * d[2] * d[0] })
+        .Select(a => a[0] + a[1] + a[2] + a.Min() / 2)
+        .Sum();
+
+    protected override int Solve2() => _input
+        .Select(l => l.Split('x').Select(int.Parse).OrderBy(s => s).ToArray())
+        .Select(d => d[0] + d[0] + d[1] + d[1] + d[0] * d[1] * d[2])
+        .Sum();
+}
